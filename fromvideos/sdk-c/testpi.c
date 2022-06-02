7f7d9893a5f8330dfb7510297dcc3a341957dcca
transvidium <55898673+transvidium@users.noreply.github.com>
2022-06-01 21:23:04 -0400

    Create testpi.c

---
+++ fromvideos/sdk-c/testpi.c
@@ -0,0 +1,26 @@
+/*
+Test of the Raspberry Pi pico C/C++ SDK
+By Samples & Tests
+16 May 2022
+# Ver 0.2
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+int testint;
+    stdio_init_all();
+	while(true){
+    		puts("Hello, world!");
+	}
+puts("press any key");
+testint = getchar();
+
+
+
+    return 0;
+}
